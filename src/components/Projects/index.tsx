e67c50f5efafbac7e9e60814bbdfddc5d6f157dd
Leandro Farias <leandrorf1606@gmail.com>
2024-12-03 21:19:49 -0300

    refatorando

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -4,54 +4,9 @@ import { Pagination } from "swiper/modules";
 
 import planet from "../../assets/quantum-moon.png";
 import astronaut from "../../assets/astronaut.png";
-
-import cinevault from "../../assets/cinevault-macbook.png";
-import ecommerce from "../../assets/e-commerce-macbook.png";
-import portfolio from "../../assets/portfolio-macbook.png";
-import searchAPI from "../../assets/searchAPI-macbook.png";
-import todoList from "../../assets/todoList-macbook.png";
-import weather from "../../assets/weatherApp-macbook.png";
+import { slides } from "./projectList";
 
 export function Projects() {
-  const slides = [
-    {
-      title: "Projetos",
-      description: "Aqui estão alguns dos meus projetos passados",
-      site: "https://cinevault-opal.vercel.app/",
-      img: cinevault,
-    },
-    {
-      title: "Projetos",
-      description: "Aqui estão alguns dos meus projetos passados",
-      site: "https://e-commerce-facul.vercel.app/",
-      img: ecommerce,
-    },
-    {
-      title: "Projetos",
-      description: "Aqui estão alguns dos meus projetos passados",
-      site: "https://weather-app-blond-nine-29.vercel.app/",
-      img: weather,
-    },
-    {
-      title: "Projetos",
-      description: "Aqui estão alguns dos meus projetos passados",
-      site: "https://to-do-list-zeta-roan.vercel.app/",
-      img: todoList,
-    },
-    {
-      title: "Projetos",
-      description: "Aqui estão alguns dos meus projetos passados",
-      site: "https://leandro-portfolio-topaz.vercel.app/",
-      img: portfolio,
-    },
-    {
-      title: "Projetos",
-      description: "Aqui estão alguns dos meus projetos passados",
-      site: "https://leandro-mfarias.github.io/search-github-api/",
-      img: searchAPI,
-    },
-  ];
-
   return (
     <section className="h-[60vh]">
       <div className="">
---
+++ src/components/Projects/projectList.ts
@@ -0,0 +1,46 @@
+import cinevault from "../../assets/cinevault-macbook.png";
+import ecommerce from "../../assets/e-commerce-macbook.png";
+import portfolio from "../../assets/portfolio-macbook.png";
+import searchAPI from "../../assets/searchAPI-macbook.png";
+import todoList from "../../assets/todoList-macbook.png";
+import weather from "../../assets/weatherApp-macbook.png";
+
+export const slides = [
+  {
+    title: "Projetos",
+    description: "Aqui estão alguns dos meus projetos passados",
+    site: "https://cinevault-opal.vercel.app/",
+    img: cinevault,
+  },
+  {
+    title: "Projetos",
+    description: "Aqui estão alguns dos meus projetos passados",
+    site: "https://e-commerce-facul.vercel.app/",
+    img: ecommerce,
+  },
+  {
+    title: "Projetos",
+    description: "Aqui estão alguns dos meus projetos passados",
+    site: "https://weather-app-blond-nine-29.vercel.app/",
+    img: weather,
+  },
+  {
+    title: "Projetos",
+    description: "Aqui estão alguns dos meus projetos passados",
+    site: "https://to-do-list-zeta-roan.vercel.app/",
+    img: todoList,
+  },
+  {
+    title: "Projetos",
+    description: "Aqui estão alguns dos meus projetos passados",
+    site: "https://leandro-portfolio-topaz.vercel.app/",
+    img: portfolio,
+  },
+  {
+    title: "Projetos",
+    description: "Aqui estão alguns dos meus projetos passados",
+    site: "https://leandro-mfarias.github.io/search-github-api/",
+    img: searchAPI,
+  },
+];
+
