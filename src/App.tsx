9a7f356ebb96dae49abaa45020ebdbb63f0183d4
Leandro Farias <leandrorf1606@gmail.com>
2024-12-05 10:14:37 -0300

    adicionando skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { About } from "./components/About";
 import { Hero } from "./components/Hero";
 import { Projects } from "./components/Projects";
+import { Skills } from "./components/Skills";
 import { MenuProvider } from "./context/menuContext";
 
 export function App() {
@@ -10,6 +11,7 @@ export function App() {
         <Hero />
         <About />
         <Projects />
+        <Skills />
       </div>
     </MenuProvider>
   );
---
+++ src/components/Skills/index.tsx
@@ -0,0 +1,41 @@
+
+
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa6";
+import { RiJavascriptFill } from "react-icons/ri";
+import { BiLogoTypescript } from "react-icons/bi";
+import { FaReact } from "react-icons/fa";
+import { FaGitAlt } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa";
+import { SiJest } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiSwagger } from "react-icons/si";
+import { BiLogoPostgresql } from "react-icons/bi";
+
+const list = [
+  { name: "React JS", icon: <FaReact className="text-sky-500"/> },
+  { name: "JavaScript", icon: <RiJavascriptFill className="text-yellow-400"/> },
+  { name: "Tailwind CSS", icon: <RiTailwindCssFill className="text-sky-500" /> },
+  { name: "Jest", icon: <SiJest className="text-rose-700" /> },
+  { name: "GIT", icon: <FaGitAlt className="text-orange-600" /> },
+  { name: "Github", icon: <FaGithub className="text-purple-600" /> },
+  { name: "TypeScript", icon: <BiLogoTypescript className="text-blue-500" /> },
+  { name: "Node.Js", icon: <FaNodeJs className="text-lime-500" /> },
+  { name: "HTML5", icon: <FaHtml5 className="text-orange-600" /> },
+  { name: "CSS3", icon: <FaCss3Alt className="text-blue-600" /> },
+  { name: "PostgreSQL", icon: <BiLogoPostgresql className="text-blue-700" /> },
+  { name: "Swagger", icon: <SiSwagger className="text-lime-400" /> },
+];
+
+export function Skills() {
+  return (
+    <div className="flex space-x-20">
+        {list.map((item) => (
+            <div className="flex flex-col items-center space-y-7">
+              <span className="text-8xl">{item.icon}</span>
+            </div>
+        ))}
+    </div>
+  );
+}
